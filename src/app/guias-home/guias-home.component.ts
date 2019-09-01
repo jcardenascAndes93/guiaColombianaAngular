7b571a96a571ad7e94701d51c0cdb2c26b433a79
SergioAndes <54184733+SergioAndes@users.noreply.github.com>
2019-09-01 13:14:12 -0500

    creacion de componente guiaHome

---
+++ src/app/guias-home/guias-home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-guias-home',
+  templateUrl: './guias-home.component.html',
+  styleUrls: ['./guias-home.component.css']
+})
+export class GuiasHomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
